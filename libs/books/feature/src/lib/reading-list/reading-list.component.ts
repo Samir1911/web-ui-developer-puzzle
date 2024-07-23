94c8b9a3e9978649d8b5e05f4a1ec6680ed01228
samir1911 <samirabdul19jr1a@gmail.com>
2024-07-23 11:05:58 +0530

    removing the book from reading list.

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { addToReadingList, getAllBooks, getReadingList, ReadingListBook, removeFromReadingList } from '@tmo/books/data-access';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Book, ReadingListItem } from '@tmo/shared/models';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -10,9 +12,28 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
 
-  constructor(private readonly store: Store) {}
+  constructor(private readonly store: Store, private snackbar: MatSnackBar) {}
 
   removeFromReadingList(item) {
+    let snackbarRef = this.snackbar.open("Book removed from the reading list.", "Undo", {duration:3000});
+    let books: ReadingListBook[];
+    this.store.select(getAllBooks).subscribe(items=>{
+      books = items;
+    });
+
     this.store.dispatch(removeFromReadingList({ item }));
+
+    snackbarRef.onAction().subscribe(()=>{
+      let book : Book ={
+        authors: item.authors,
+        id: item.bookId,
+        coverUrl: item.coverUrl,
+        description: item.description,
+        publishedDate:item.publishedDate,
+        publisher: item.publisher,
+        title: item.title
+      }
+      this.store.dispatch(addToReadingList({book}))
+    });
   }
 }
